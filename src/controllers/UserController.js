66e3799798d0ff6591b917b1ba744e8c1ef3b338
unknown <gcolares95@gmail.com>
2022-03-13 15:34:06 -0300

    Listando todos usuários

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,7 +2,16 @@ const User = require('../model/User');
 
 // lida com as requisições e respostas
 module.exports = {
+  async index (req, res) {
+    // buscar todos usuários
+    const users = await User.findAll();
+
+    return res.json(users);
+  },
+
+
   async store(req, res) {
+    // inserir usuário
     const { name, email } = req.body;
 
     const user = await User.create({ name, email })  // await frente porque toda manipulação com BD é assincrona
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ const UserController = require('./controllers/UserController');
 
 const routes = express.Router()
 
-// rota para cadastro de usuários
- routes.post('/users', UserController.store);
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
+
 
 module.exports = routes
