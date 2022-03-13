8e42c5769b3f6107043f45743d0a78397040843c
unknown <gcolares95@gmail.com>
2022-03-13 15:48:02 -0300

    Foreign key

---
+++ src/database/migrations/20220313183720-create-addresses.js
@@ -0,0 +1,49 @@
+'use strict'
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('addresses', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE', // CASCADE -> sempre que há uma alteração no relacionamento, fará também quem usa aquela coluna como referencia
+        onDelete: 'CASCADE',
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      zipcode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('addresses')
+  }
+}
